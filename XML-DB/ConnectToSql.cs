bca35c5f216c424bf37be2e113a5d6b81aaba4c3
Michał <mmichalf@gmail.com>
2013-06-16 17:19:56 +0200

    nic nowego

--- XML-DB/ConnectToSql.cs
+++ XML-DB/ConnectToSql.cs
@@ -11,12 +11,21 @@ namespace XML_DB
     public class ConnectToSql
     {
         public List<string> TableList;
+        
+
         public ConnectToSql(string pathToFile)
         {
             var connection = new SqlCeConnection(@"Data Source=" + pathToFile + ";password=xml-db123");
 
             connection.Open();
 
+            ReadTablesFrom(connection);
+
+
+        }
+
+        private void ReadTablesFrom(SqlCeConnection connection)
+        {
             var tablesQuery = new SqlCeCommand("SELECT TABLE_NAME FROM information_schema.tables", connection);
 
             var dataAdapter = new SqlCeDataAdapter(tablesQuery);
@@ -25,7 +34,11 @@ namespace XML_DB
             dataAdapter.Fill(ds);
 
             TableList = ManageMsSqlDb.ReadTablesFrom(ds).ToList();
+        }
 
+        public void GetDataFromTable(string tableName)
+        {
+            
         }
     }
 }
