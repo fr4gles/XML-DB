800846aa4ab6c418d602ea694bc13129fa01fc30
Michał <mmichalf@gmail.com>
2013-06-16 16:14:24 +0200

    zczytywanie nazw tabel

--- XML-DB/ConnectToSql.cs
+++ XML-DB/ConnectToSql.cs
@@ -10,20 +10,22 @@ namespace XML_DB
 {
     public class ConnectToSql
     {
+        public List<string> TableList;
         public ConnectToSql(string pathToFile)
         {
             var connection = new SqlCeConnection(@"Data Source=" + pathToFile + ";password=xml-db123");
 
             connection.Open();
 
-            var tables = new SqlCeCommand("SELECT TABLE_NAME FROM information_schema.tables", connection);
+            var tablesQuery = new SqlCeCommand("SELECT TABLE_NAME FROM information_schema.tables", connection);
 
-            
-
-            var dataAdapter = new SqlCeDataAdapter(tables);
+            var dataAdapter = new SqlCeDataAdapter(tablesQuery);
 
             var ds = new DataSet();
             dataAdapter.Fill(ds);
+
+            TableList = ManageMsSqlDb.ReadTablesFrom(ds).ToList();
+
         }
     }
 }
---
+++ XML-DB/ManageMsSqlDb.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XML_DB
+{
+    public static class ManageMsSqlDb
+    {
+        public static IEnumerable<string> ReadTablesFrom(DataSet ds)
+        {
+            foreach (DataTable dt in ds.Tables)
+            {
+                foreach (DataRow dr in dt.Rows)
+                {
+                    foreach (DataColumn column in dt.Columns)
+                    {
+                        yield return dr[column.ToString()].ToString();
+                    }
+                }
+            }
+        }
+
+        public static void ReadStructureFrom()
+        {
+            
+        }
+    }
+}
