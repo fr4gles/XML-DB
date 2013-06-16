8987e35660fd3b779948702c9e557c5cd0c713c7
silgur <zbigniew.bielesz@gmail.com>
2013-06-16 18:13:47 +0200

    Ustawienia

--- XML-DB/MainWindow.xaml.cs
+++ XML-DB/MainWindow.xaml.cs
@@ -15,6 +15,9 @@ using System.Windows.Shapes;
 using Microsoft.Win32;
 using System.Data.SqlClient;
 using System.Data.SqlServerCe;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 
 namespace XML_DB
@@ -24,104 +27,50 @@ namespace XML_DB
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public Settings mainSettings = null;
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void button_openXmlFile_Click(object sender, RoutedEventArgs e)
-        {
-            textBox_main.Text = "";
-
-            var openDialog = new OpenFileDialog
-            {
-                Filter = "xml files (*.xml)|*.xml|All files (*.*)|*.*",
-                InitialDirectory = Environment.CurrentDirectory,
-                Title = "Wybierz plik xml"
-            };
-
-            if (!openDialog.ShowDialog().Value) return;
-
-            var pathToFile = openDialog.FileName;
-            textBox_pathToXML.Text = pathToFile;            
-
-            var result = new XmlParseAndRead(pathToFile);
-            var xmlResult = new XmlToSql(result.ReadTableName());
-
-
-
-
-            textBox_main.Text = xmlResult.MakeSqlCreateDbCommandFrom(result.ReadStructure());
-
-            textBox_main.Text += "\n\n";
-
-            textBox_main.Text += xmlResult.MakeSqlInsertValuesDbCommandFrom(result.ReadRecords());
-
-
-
-        }
-
-        private void button_openSdfFile_Click(object sender, RoutedEventArgs e)
-        {
-            textBox_main.Text = "";
-
-            var openDialog = new OpenFileDialog
-            {
-                Filter = "xml files (*.sdf)|*.sdf|All files (*.*)|*.*",
-                InitialDirectory = Environment.CurrentDirectory,
-                Title = "Wybierz plik sdf"
-            };
-
-            if (!openDialog.ShowDialog().Value) return;
-
-            var pathToFile = openDialog.FileName;
-            textBox_pathToSDF.Text = pathToFile;
-
-            var sql = new ConnectToSql(pathToFile);
-
-
-        }
-
-        private void buttonLaunch_Click(object sender, RoutedEventArgs e)
-        {
-            var openDialog = new OpenFileDialog
-            {
-                Filter = "xml files (*.sdf)|*.sdf|All files (*.*)|*.*",
-                InitialDirectory = Environment.CurrentDirectory,
-                Title = "Wybierz plik sdf"
-            };
-
-            if (!openDialog.ShowDialog().Value) return;
-
-            var pathToFile = openDialog.FileName;
             
-
-            var connection = new SqlCeConnection(@"Data Source=" + pathToFile + ";password=xml-db123");
-            connection.Open();            
-
-            SqlCeCommand cmd = connection.CreateCommand();
-
-            cmd.CommandText = "select * from TestTable";
-            SqlCeDataReader reader = cmd.ExecuteReader();
-
-
-
-
-            string ret = "";   
-            while (reader.Read())
+            if (System.IO.File.Exists("settingsFile.obj"))
             {
-               int j=0;                       
-                
-                for (int i = 0; i < reader.FieldCount; i++)
-                {
-                    ret += "\nCol name:: " + reader.GetName(i) + " Col type: " + reader.GetDataTypeName(i);                    
-                }                            
-               
+                mainSettings = LoadSettingsFromFile("settingsFile.obj");
             }
-            MessageBox.Show(ret);        
+            else
+            {                
+                MessageBox.Show("Nie wykryto pliku ustawień bazy danych. Wprowadź ustawienia");
+                var newWindow = new SettingsWindow();
+                newWindow.ShowDialog();
+                mainSettings = LoadSettingsFromFile("settingsFile.obj");                 
+            }
+            MessageBox.Show(mainSettings.databasePath);//do testowania
+            MessageBox.Show(mainSettings.password);//jw
+
             
         }
 
+        public Settings LoadSettingsFromFile(string settingsFilePath)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream(settingsFilePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+            Settings obj = (Settings)formatter.Deserialize(stream);
+            stream.Close();
+            return obj;
+            
+        }
+
+        public void SaveSettingsToFile(Settings tempSettings,string settingsFilePath)
+        {                     
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream(settingsFilePath, FileMode.Create, FileAccess.Write, FileShare.None);
+            formatter.Serialize(stream, tempSettings);
+            stream.Close();
+        }
+
+
+
+      
         private void buttonXmlToDb_Click(object sender, RoutedEventArgs e)
         {
 
@@ -137,13 +86,16 @@ namespace XML_DB
 
         private void buttonSettings_Click(object sender, RoutedEventArgs e)
         {
+            var newWindow = new SettingsWindow();
+            newWindow.ShowDialog();
 
         }
 
         private void buttonCommand_Click(object sender, RoutedEventArgs e)
         {
             var newWindow = new LaunchSqlCommand();
-            newWindow.Show(); 
+            newWindow.Show();
+            mainSettings = LoadSettingsFromFile("settingsFile.obj");     
         }
     }
 }
---
+++ XML-DB/Settings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XML_DB
+{
+    [Serializable]
+    public class Settings
+    {
+        public string databasePath;
+        public string password;
+
+        public Settings(string path, string pass)
+        {
+            password = pass;
+            databasePath = path;
+        }
+    }
+}
