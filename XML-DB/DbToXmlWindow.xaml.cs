9bc96a9e64b6d0f32fe1b73cd84bd639a19585ea
silgur <zbigniew.bielesz@gmail.com>
2013-06-16 15:34:43 +0200

    Nowe okna ( zero estetyki)

---
+++ XML-DB/DbToXmlWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace XML_DB
+{
+    /// <summary>
+    /// Interaction logic for DbToXmlWindow.xaml
+    /// </summary>
+    public partial class DbToXmlWindow : Window
+    {
+        public DbToXmlWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_openSdfFile_Click(object sender, RoutedEventArgs e)
+        {
+            
+            var openDialog = new OpenFileDialog
+            {
+                Filter = "xml files (*.sdf)|*.sdf|All files (*.*)|*.*",
+                InitialDirectory = Environment.CurrentDirectory,
+                Title = "Wybierz plik sdf"
+            };
+
+            if (!openDialog.ShowDialog().Value) return;
+
+            var pathToFile = openDialog.FileName;
+            textBox_pathToSDF.Text = pathToFile;
+
+            var sql = new ConnectToSql(pathToFile);
+
+        }
+    }
+}
---
+++ XML-DB/XmlToDbWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace XML_DB
+{
+    /// <summary>
+    /// Interaction logic for XmlToDbWindow.xaml
+    /// </summary>
+    public partial class XmlToDbWindow : Window
+    {
+        public XmlToDbWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_openXmlFile_Click(object sender, RoutedEventArgs e)
+        {
+            textBox_main.Text = "";
+
+
+
+            var openDialog = new OpenFileDialog
+            {
+                Filter = "xml files (*.xml)|*.xml|All files (*.*)|*.*",
+                InitialDirectory = Environment.CurrentDirectory,
+                Title = "Wybierz plik xml"
+            };
+
+            if (!openDialog.ShowDialog().Value) return;
+
+            var pathToFile = openDialog.FileName;
+            textBox_pathToXML.Text = pathToFile;
+
+            webBrowserXml.Navigate(pathToFile);
+
+            var result = new XmlParseAndRead(pathToFile);
+            var xmlResult = new XmlToSql(result.ReadTableName());
+
+            textBox_main.Text = xmlResult.MakeSqlCreateDbCommandFrom(result.ReadStructure());
+            textBox_main.Text += "\n\n";
+            textBox_main.Text += xmlResult.MakeSqlInsertValuesDbCommandFrom(result.ReadRecords());
+
+        }
+
+        private void buttonSave_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+
+
+        }
+    }
+}
